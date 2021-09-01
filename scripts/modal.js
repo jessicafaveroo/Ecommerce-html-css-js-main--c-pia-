e8d2f8e9d9983ae03efda22d60b22a8aed97417b
SergioBarbosa98 <sergioubira@outlook.com>
2021-09-01 10:43:43 -0300

    "Show the product"

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,10 @@
+const modalWrapper = document.querySelector(".modal-wrapper")
+const show = document.querySelector("#content .prod1 .prod12 button")
 
+function ativar(){
+    modalWrapper.classList.add("active")
+}
+
+function desativar(){
+    modalWrapper.classList.remove("active")
+}
